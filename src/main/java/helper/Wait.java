ba98dd3b0b91ef8fb6f62e78aec053c3df3d3bb7
DangD96 <35606061+DangD96@user.noreply.github.com>
2023-11-19 23:35:43 -0600

    make wait func a helper func

---
+++ src/main/java/helper/Wait.java
@@ -0,0 +1,15 @@
+package helper;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class Wait {
+    public static WebElement forElement(WebDriver driver, By byMethod) {
+        return new WebDriverWait(driver, Duration.ofSeconds(5)).until(ExpectedConditions.visibilityOfElementLocated(byMethod));
+    }
+}
