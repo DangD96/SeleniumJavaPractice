fc25a43f6936318038ca200a630ff94f8934abb8
DangD96 <35606061+DangD96@user.noreply.github.com>
2023-11-19 23:15:33 -0600

    refactor element wait function

--- src/main/java/Assignment4.java
+++ src/main/java/Assignment4.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -13,15 +14,14 @@ public class Assignment4 {
     public static void main (String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        Duration wait = Duration.ofSeconds(5);
-        driver.manage().timeouts().implicitlyWait(wait);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 
         try {
             driver.get("https://the-internet.herokuapp.com/");
-            new WebDriverWait(driver, wait).until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Multiple Windows"))).click();
+            waitForElement(driver, By.linkText("Multiple Windows")).click();
 
             // Open up child window
-            new WebDriverWait(driver, wait).until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Click Here"))).click();
+            waitForElement(driver, By.linkText("Click Here")).click();
 
             // Get window handles
             Set<String> windowHandles = driver.getWindowHandles();
@@ -31,20 +31,26 @@ public class Assignment4 {
 
             // Switch to child window and print out text
             driver.switchTo().window(childWindowID);
-            printHeaderContents(driver, wait);
+            printHeaderContents(driver);
 
             // Switch back to parent window and print out text
             driver.switchTo().window(parentWindowID);
-            printHeaderContents(driver, wait);
+            printHeaderContents(driver);
+
         } catch(Exception e) {
             System.out.println("Error: " + Arrays.toString(e.getStackTrace()));
+
         } finally {
             driver.quit();
         }
     }
 
-    public static void printHeaderContents(WebDriver driver, Duration wait) {
-        String contents = new WebDriverWait(driver,wait).until(ExpectedConditions.visibilityOfElementLocated(By.tagName("h3"))).getText();
+    public static void printHeaderContents(WebDriver driver) {
+        String contents = waitForElement(driver, By.tagName("h3")).getText();
         System.out.println(contents);
+    }
+    public static WebElement waitForElement(WebDriver driver, By byMethod) {
+        // https://www.selenium.dev/selenium/docs/api/java/org/openqa/selenium/support/ui/ExpectedConditions.html
+        return new WebDriverWait(driver, Duration.ofSeconds(5)).until(ExpectedConditions.visibilityOfElementLocated(byMethod));
     }
 }
